3fd67ae0ff54518171bf2cdcef61c68d4d24d4e9
Li Kui <90845831+likui628@users.noreply.github.com>
2023-01-10 08:08:30 +0000

    exercises `standard_library_types`

--- exercises/standard_library_types/box1.rs
+++ exercises/standard_library_types/box1.rs
@@ -16,13 +16,13 @@
 //
 // Execute `rustlings hint box1` for hints :)
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
+
+use crate::List::{Cons, Nil};
 
 fn main() {
     println!("This is an empty cons list: {:?}", create_empty_list());
@@ -33,11 +33,11 @@ fn main() {
 }
 
 pub fn create_empty_list() -> List {
-    unimplemented!()
+    Nil
 }
 
 pub fn create_non_empty_list() -> List {
-    unimplemented!()
+    Cons(1, Box::new(Cons(4, Box::new(Nil))))
 }
 
 #[cfg(test)]
--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,7 +1,5 @@
 // iterators4.rs
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
     // Do not use:
@@ -12,6 +10,8 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    let numbers = (1..num + 1).collect::<Vec<u64>>();
+    numbers.iter().fold(1, |acc, x| acc * x)
 }
 
 #[cfg(test)]
